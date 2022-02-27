9dee8f5655cdf6e3d31071dc9da15fa6b3033545
miris-mimitako <miris.mikitako@gmail.com>
2022-02-27 21:46:16 +0900

    Change: css modules

--- app-miris-trial/pages/index.tsx
+++ app-miris-trial/pages/index.tsx
@@ -1,33 +1,23 @@
-import Link from 'next/link'
 import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
-import type { NextPage } from 'next'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-function HomePage () {
+export default function Home() {
   return (
-      <>
-
-        <Head>
-          <title>Create Next App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
-        <header>
-          <p className="title">
-            Read {' '}
-            <Link href="/posts/first-post"> 
-              <a>this page!</a>
-            </Link>
-          </p>
-        </header>
-      </>
-    )
-  
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - youll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
 }
-
-export default HomePage;
 
 // learn:
 // Link uses Link tag not use a tag. An a tag indicates link word.
