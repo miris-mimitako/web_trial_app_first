4b19aa2d1de9b936e855eb94e991b0f3157c8744
miris-mimitako <miris.mikitako@gmail.com>
2022-03-02 23:38:03 +0900

    add: id js

--- app-miris-trial/lib/posts.js
+++ app-miris-trial/lib/posts.js
@@ -1,3 +1,5 @@
+import { remark } from 'remark'
+import html from 'remark-html'
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
@@ -5,12 +7,12 @@ import matter from 'gray-matter'
 
 const postsDirectory = path.join(process.cwd(), './pages/posts') // This cannot be worked public document.
 
-export async function getSortedPostsData() {
-      // Instead of the file system,
-    // fetch post data from an external API endpoint
-    const res = await fetch('..')
-    return res.json()
-}
+// export async function getSortedPostsData() {
+//       // Instead of the file system,
+//     // fetch post data from an external API endpoint
+//     const res = await fetch('..')
+//     return res.json()
+// }
 
 export function getSortedPostsData() {
   // Get file names under /posts
@@ -40,4 +42,50 @@ export function getSortedPostsData() {
         return -1
         }
     })
+}
+
+
+export function getAllPostIds(){
+    const fileNames = fs.readdirSync(postsDirectory)
+    // Returns an array that looks like this:
+    // [
+    //   {
+    //     params: {
+    //       id: 'ssg-ssr'
+    //     }
+    //   },
+    //   {
+    //     params: {
+    //       id: 'pre-rendering'
+    //     }
+    //   }
+    // ]
+    return fileNames.map(fileName => {
+        return {
+        params: {
+            id: fileName.replace(/\.md$/, '')
+        }
+        }
+    })
+}
+
+export async function getPostData(id) {
+    const fullPath = path.join(postsDirectory, `${id}.md`)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents)
+
+    // Use remark to convert markdown into HTML string
+    const processedContent = await remark()
+        .use(html)
+        .process(matterResult.content)
+    const contentHtml = processedContent.toString()
+
+    // Combine the data with the id and contentHtml
+    return {
+        id,
+        contentHtml,
+        ...matterResult.data
+    }
 }
