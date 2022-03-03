22952d262f20c8de28894864c41cd5dead9473c9
miris-mimitako <miris.mikitako@gmail.com>
2022-03-03 15:08:35 +0900

    add: date.js

--- app-miris-trial/pages/posts/[id].js
+++ app-miris-trial/pages/posts/[id].js
@@ -1,15 +1,23 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import Head from 'next/head'
 
 
 export default function Post({ postData }) {
     return (
         <Layout>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            {postData.date}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+            </article>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
     )
 }
@@ -23,7 +31,8 @@ export async function getStaticPaths() {
     }
 }
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id)
+      // Add the "await" keyword like this:
+    const postData = await getPostData(params.id)
     return {
         props: {
         postData
