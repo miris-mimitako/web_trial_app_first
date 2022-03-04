dc7d13da2b08e451f76c8ba92ccdb5ab3c77d4e2
miris-mimitako <miris.mikitako@gmail.com>
2022-03-04 09:28:21 +0900

    delete: wp cms

--- cms-wordpress-app/next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  images: {
-    domains: [
-      // "[yourapp].wpengine.com" (Update this to be your Wordpress application name in order to load images connected to your posts)
-      'secure.gravatar.com',
-    ],
-  },
-}
